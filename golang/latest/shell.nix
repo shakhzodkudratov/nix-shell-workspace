23e81b86ba531455070cbaecee21d5ab7f68666c
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-18 11:07:00 +0500

    feat: ginkgo package

--- golang/latest/shell.nix
+++ golang/latest/shell.nix
@@ -1,6 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }: pkgs.mkShell {
+{ pkgs ? let 
+  nixpkgs-unstable = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  };
+  in import nixpkgs-unstable {}
+}: pkgs.mkShell {
   buildInputs = [
     pkgs.go
+    pkgs.ginkgo
   ];
 
   GOROOT = "${pkgs.go}/share/go";
